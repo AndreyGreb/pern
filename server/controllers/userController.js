273f36df7d1177b0b8de2ec096aef605a22811e0
Andrey <andrey.grebeshov@gmail.com>
2021-05-04 14:24:44 +0300

    настройка входа в систему, токен

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,25 +1,47 @@
 const ApiError = require('../error/ApiError')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const { User, Basket } = require('../models/models')
 
 class UserController {
-    async registration(req, res) {
+    async registration(req, res, next) {
+        const { email, password, role } = req.body
 
+        if (!email || !password) {
+            return next(ApiError.badRequest('Некорректный email или пароль!'))
+        }
 
+        const candidate = await User.findOne({ where: { email } })
+
+        if (candidate) {
+            return next(ApiError.badRequest('Пользователь с таким email уже существует!'))
+        }
+
+        const hasPassword = await bcrypt.hash(password, 5)
+        const user = await User.create({ email, role, password: hasPassword })
+        const basket = await Basket.create({ userId: user.id })
+        const token = jwt.sign({ id: user.id, email, role },
+            process.env.SECRET_KEY,
+            { expiresIn: '24h' }
+        )
+
+        return res.json({token})
     }
 
     async login(req, res) {
 
-        
+
     }
 
     async check(req, res, next) {
-        const {id} = req.query
+        const { id } = req.query
 
-        if(!id) {
+        if (!id) {
             return next(ApiError.badRequest('Не задан ID'))
         }
 
         res.json(id)
-                
+
     }
 }
 
