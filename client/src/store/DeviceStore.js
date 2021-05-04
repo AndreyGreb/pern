2ac9a1755df375747c05c27bcf18aec4ced88921
Andrey <andrey.grebeshov@gmail.com>
2021-05-04 19:27:19 +0300

    верстка navbar

--- client/src/store/DeviceStore.js
+++ client/src/store/DeviceStore.js
@@ -6,11 +6,11 @@ export default class DeviceStore {
             {id: 1, name: "Холодильники"},
             {id: 2, name: "Смартфоны"},
         ]
-        this.brands = [
+        this._brands = [
             {id: 1, name: "Samsung"},
             {id: 2, name: "Apple"},
         ]
-        this.devices = [
+        this._devices = [
             {id: 1, name: "12 pro", price: 34567, raiting: 0, img: "http://localhost:5000/76849c15-60de-4330-a729-10dbe82af735.jpg"},
             {id: 2, name: "a515", price: 12345, raiting: 0, img: "http://localhost:5000/4617a866-2cf2-487c-8195-90d92b216ac2.jpg"},
             {id: 3, name: "note pro", price: 4321, raiting: 0, img: "http://localhost:5000/dc4707c9-688b-43b3-91b4-c59628e826f7.jpg"},
