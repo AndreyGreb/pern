01fed5e467a6792d50b63d22bc5c8192f61e8b0d
Andrey <andrey.grebeshov@gmail.com>
2021-05-05 00:34:15 +0300

    верстка карточки товара

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -2,22 +2,35 @@ import React, { useContext } from 'react'
 import { Context } from '..'
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
-import {NavLink} from 'react-router-dom'
+import { NavLink, useHistory } from 'react-router-dom'
 import { Button, Container } from 'react-bootstrap'
-import { SHOP_ROUTE } from '../utils/consts';
+import { ADMIN_ROUTE, LOGIN_ROUTE, SHOP_ROUTE } from '../utils/consts';
 import { observer } from 'mobx-react-lite'
 
 const NavBar = observer(() => {
 
     const { user } = useContext(Context)
+    const history = useHistory()
+
     return (
         <Navbar bg="dark" variant="dark">
             <Container>
                 <NavLink to={SHOP_ROUTE} style={{ color: 'white' }}>КупиДевайс</NavLink>
                 {user._isAuth ?
                     <Nav className="ml-auto" style={{ color: 'white' }}>
-                        <Button variant={"outline-light"}>Админ панель</Button>
-                        <Button variant={"outline-light"} className="ml-2">Войти</Button>
+                        <Button
+                            variant={"outline-light"}
+                            onClick={() => history.push(ADMIN_ROUTE)}
+                        >
+                            Админ панель
+                        </Button>
+                        <Button
+                            variant={"outline-light"}
+                            className="ml-2"
+                            onClick={() => history.push(LOGIN_ROUTE)}
+                        >
+                            Выйти
+                        </Button>
                     </Nav>
                     :
                     <Nav className="ml-auto" style={{ color: 'white' }}>
--- client/src/pages/DevicePage.js
+++ client/src/pages/DevicePage.js
@@ -1,10 +1,59 @@
 import React from 'react'
+import { Button, Card, Col, Container, Image, Row } from 'react-bootstrap'
 
 const DevicePage = () => {
+
+    const device = { id: 1, name: "12 pro", price: 34567, raiting: 0, img: "http://localhost:5000/76849c15-60de-4330-a729-10dbe82af735.jpg" }
+    const description = [
+        { id: 1, title: 'Оперативная память', description: '5 гб' },
+        { id: 2, title: 'Камера', description: '12 мп' },
+        { id: 3, title: 'Процессор', description: 'Пентиум 3' },
+        { id: 4, title: 'Кол-во ядер', description: '2' },
+        { id: 5, title: 'Аккумулятор', description: '4000' },
+    ]
+
     return (
-        <div>
-            DevicePage
-        </div>
+        <Container className="mt-3">
+            <Row>
+
+
+                <Col md={4}>
+                    <Image width={300} height={300} src={device.img} />
+                </Col>
+
+                <Col md={4}>
+                    <Row className="d-flex flex-column align-items-center">
+                        <h2>{device.name}</h2>
+                    </Row>
+                </Col>
+
+                <Col md={4}>
+                    <Card
+                        className="d-flex flex-column align-items-center justify-content-around"
+                        style={{ width: 300, height: 300, fontSize: 32, border: '5px solid lightgray' }}
+                    >
+                        <h3>{device.price} руб</h3>
+                        <Button variant={"outline-dark"}>Добавить в корзину</Button>
+                    </Card>
+                </Col>
+
+                <Col md={4}>
+
+                </Col>
+            </Row>
+
+            <Row className="d-flex flex-column m-3">
+                <h1>Характеристики</h1>
+                {description.map((info, index) =>
+                    <Row
+                        key={info.id}
+                        style={{ background: index % 2 === 0 ? 'lightgray' : 'transpare', padding: 10 }}
+                    >
+                        {info.title}: {info.description}
+                    </Row>
+                )}
+            </Row>
+        </Container>
     )
 }
 
